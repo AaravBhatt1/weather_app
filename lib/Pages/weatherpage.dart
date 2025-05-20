1891689c7ef2a767fb5177861d8178a0ef3faa56
GingerBot2000 <78940510+GingerBot2000@users.noreply.github.com>
2025-05-20 14:12:06 +0100

    stuff

--- lib/Pages/weatherpage.dart
+++ lib/Pages/weatherpage.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
 
-class WeatherPage extends StatelessWidget {
+class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  List<String> cities = ['New York', 'London', 'Tokyo', 'Sydney'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // TODO: Use reorderable list view
-      body: ListView(
-        padding: EdgeInsets.all(30),
+      body: ReorderableListView(
+        padding: const EdgeInsets.all(30),
         children: [
-          Text("Stuff will go here", style: Theme.of(context).textTheme.headlineLarge,)
-        ]
+          for (final city in cities)
+            ListTile(
+              key: ValueKey(city), // Unique key is required
+              title: Text(city),
+              // leading: const Icon(Icons.drag_handle),
+            ),
+        ],
+        onReorder: (oldIndex, newIndex) {
+          setState(() {
+            if (newIndex > oldIndex) newIndex -= 1;
+            final item = cities.removeAt(oldIndex);
+            cities.insert(newIndex, item);
+          });
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          // You can add logic to add a new city
+        },
         child: const Icon(Icons.add),
       ),
     );
