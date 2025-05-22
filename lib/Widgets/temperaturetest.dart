075414a3d2b67f947746db9d791a05d0decaae70
aaravbhatt <aarav.bhatt.uk@gmail.com>
2025-05-22 14:49:07 +0100

    basic api

---
+++ lib/Widgets/temperaturetest.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class TemperatureTest extends StatelessWidget {
+  const TemperatureTest({super.key, required this.builder});
+
+  final Widget Function(BuildContext, Map) builder;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: FutureBuilder(
+          future: http.get(Uri.parse('http://api.weatherapi.com/v1/current.json?key=c48cd3f6bb9941ba83c132239252205&q=Cambridge&aqi=no')),
+          builder: (context, future) {
+            if (future.hasData) {
+              Map data = jsonDecode(future.data!.body);
+              print (data);
+              return builder(context, data);
+            }
+            return const CircularProgressIndicator();
+          }
+      )
+    );
+  }
+}
